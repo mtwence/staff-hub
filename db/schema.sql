62b9304a145f7c1caa89bbe5bc2d94c846ded723
Gleepy <107324298+mtwence@users.noreply.github.com>
2022-08-11 20:22:41 -0400

    add data models for employee department role

---
+++ db/schema.sql
@@ -0,0 +1,33 @@
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+
+USE employee_db;
+
+DROP TABLE IF EXISTS department;
+CREATE TABLE department (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) 
+);
+
+DROP TABLE IF EXISTS role;
+CREATE TABLE role(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT,
+    FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
+);
+
+DROP TABLE IF EXISTS employee;
+CREATE TABLE employee(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) ,
+    last_name VARCHAR(30),
+    role_id INT ,
+    manager_id INT DEFAULT NULL,
+    FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL,
+    FOREIGN KEY (manager_id) REFERENCES  employee(id) ON DELETE SET NULL  
+
+);
+
+
